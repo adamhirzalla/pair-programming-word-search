bfd187460ee69d2c070ddbbf3f69876479d43e80
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-03 20:12:42 -0400

    Solving Word Search PP Activity and testing

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let isFound = false;
+  // checking horizontals
+  letters.map(el => el.join('')).forEach(line => {
+    if (line.includes(word)) isFound = true;
+  });
+  // transposing
+  const transposedArray = [];
+  for (let row = 0; row < letters[0].length; row++) {
+    transposedArray.push([]);
+    for (let col = 0; col < letters.length; col++) {
+      transposedArray[row].push(letters[col][row]);
     }
-}
+  }
+  // checking verticals
+  transposedArray.map(el => el.join('')).forEach(line => {
+    if (line.includes(word)) isFound = true;
+  });
+  return isFound;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
